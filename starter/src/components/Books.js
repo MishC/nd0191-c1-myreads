45c8f52f5986f0b7817d66fc10160cbe94883866
MishC <michaela.chovancova@gmail.com>
2024-05-13 22:24:16 +0200

    Update Books.js

--- starter/src/components/Books.js
+++ starter/src/components/Books.js
@@ -11,15 +11,14 @@ const Books = ({category,selectedBooks,selectCategory})=>{
           style={{
             width: 128,
             height: 193,
-            backgroundImage:`url(${book.imageLinks.thumbnails.thumbnail})`
+            backgroundImage:`url(${book.imageLinks.thumbnail})`
         }}       
         >
         </div>    
 
         <ChooseCategory category={category} selectCategory={selectCategory} title={book.title}/></div>
       <div className="book-title">{book.title}</div>
-      <div className="book-authors">{book.authors.length>1?book.authors.map((author,i,arr)=>{
-        if (i!==arr.length-1){return`${author}, `}else {return `${author}`}}).trim():book.authors[0]}</div>
+      <div className="book-authors">{book.authors}</div>
     </div>
   </li>
    );
