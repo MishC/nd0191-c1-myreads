0d18151813fccf704fb9d3b3dee0880230013ddd
MishC <michaela.chovancova@gmail.com>
2024-05-14 11:52:24 +0200

    Update App.js

--- starter/src/App.js
+++ starter/src/App.js
@@ -1,50 +1,70 @@
 import "./App.css";
 import { useEffect, useState } from "react";
 import Search from "./components/Search.js";
-import allbooks from "./AllBooks.json";
-import {Link,Routes, Route} from "react-router-dom";
-
-import DisplayBooks from "./components/DisplayBooks.js";
+//import allBooks from "./AllBooks.json";
+import {Link,Routes, Route,useNavigate} from "react-router-dom";
+import {getAll,update} from "./BooksAPI.js";
+//import DisplayBooks from "./components/DisplayBooks.js";
+import Home from "./components/Home.js";
 
 
 function App() {
   const [showSearchPage, setShowSearchPage] = useState(false);
+  const [status, setStatus] = useState("currentlyReading");
+  const [allbooks, setAllbooks]=useState([]);
+  const [updatedBook,setUpdatedBook]=useState({});
+  const [bookId, setBookId]=useState("");
+  //const [isLoading, setIsLoading] = useState(false);
+  
+ 
     
+const getBooks =async()=>{const books=await getAll();setAllbooks([...books]) };
+
+/*const getUpdated=async(allbooks,id)=>{
+  allbooks.filter(book => book.id&&book.id === id)};*/
+
+useEffect(async()=>{await getBooks();   },[]);
+
+const selectCategory=async (status,id)=>{
+ setBookId(id);
+ setStatus(status);
+
+  if (!status || !id) {
+    console.error('Status and ID are required parameters.');
+    return;
+  };
+ setUpdatedBook( allbooks.filter(book => book.id&&book.id === bookId));
+  
+  await update(updatedBook[0],status) ;
+
+
+await getBooks();
+}; 
   
 const closeSearch=(value)=>{setShowSearchPage(value)};
+
+
 
  
   return (
     <div className="app">
 
-      {showSearchPage ? (
+      
         <Routes> 
           <Route exact path="/search" element={
             <Search closeSearch={closeSearch}/>}/>
-
+            <Route  exact path="/"
+            element={<Home  allbooks={allbooks} selectCategory={selectCategory} />}/>
             </Routes>
 
-      ) : (
-        <div className="list-books">
-          <div className="list-books-title">
-            <h1>MyReads</h1>
-          </div>
-          <div className="list-books-content">
-            <div>
-            <DisplayBooks  category="currentlyReading" allbooks={allbooks} />
-            <DisplayBooks  category="wantToRead" allbooks={allbooks}/>
-            <DisplayBooks  category="read" allbooks={allbooks} />
+     
         
-
-            </div>
-          </div>
-          <div className="open-search">
+            <div className="open-search">
             <Link to="/search"
         onClick={() => setShowSearchPage(!showSearchPage)}>Add a book</Link>
           </div>
-        </div>
-      )}
-    </div>
+          </div>
+  
   );
 }
 
