f4f3236c56b739600af3a2cf10614f680bb6d935
MishC <michaela.chovancova@gmail.com>
2024-05-14 11:53:02 +0200

    Update DisplayBooks.js

--- starter/src/components/DisplayBooks.js
+++ starter/src/components/DisplayBooks.js
@@ -3,39 +3,16 @@ import Books from "./Books";
 //import {get, getAll, update,search, addBook} from "../BooksAPI.js";
 
 
-const DisplayBooks=({category,allbooks})=>{
-    const [status, setStatus] = useState(category);
-    //const [isLoading, setIsLoading] = useState(false);
-
-
-    /*useEffect(() => {
-        getAll()
-          .then(items => {
-            const booksToAdd = allbooks.filter(book => !items.some(item => item.id === book.id));
-            booksToAdd.forEach(book=>addBook(book));
-          })
-          .then(() => {
-            console.log("Books added successfully.");
-          })
-          .catch(error => {
-            console.error("Error adding books:", error);
-          });
-      }, []);*/
-      
-
-  const selectCategory=(status,title)=>{
-    setStatus(status);
-    allbooks.filter(book=>book.title===title).forEach(book=>book.shelf=status);
-
-  };
-
+const DisplayBooks=({category,allbooks, selectCategory})=>{
+   
     
+
 return (    <div className="bookshelf">
                 <h2 className="bookshelf-title">{category}</h2>
                 <div className="bookshelf-books">{allbooks.length>1?
                   <ol className="books-grid">
                               
-                 <Books  category ={status} selectedBooks={category.length>2?allbooks.filter(book=>book.shelf===category):allbooks} selectCategory={selectCategory} />
+                 <Books  selectedBooks={category.length>1?allbooks.filter(book=>book.shelf===category):allbooks} selectCategory={selectCategory} />
                   </ol>:<ol className="books-grid"></ol>}
                 </div>
               </div>)
