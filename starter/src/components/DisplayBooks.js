34085bf7f8aaa01005cad25408a58b76913df774
MishC <michaela.chovancova@gmail.com>
2024-05-14 14:32:01 +0200

    cleaning comments

--- starter/src/components/DisplayBooks.js
+++ starter/src/components/DisplayBooks.js
@@ -1,11 +1,9 @@
 import Books from "./Books";
-//import {get, getAll, update,search, addBook} from "../BooksAPI.js";
 
 
 const DisplayBooks=({category,allbooks, selectCategory})=>{
 const separateCamelCase=(str)=> {
-    // Use a regular expression to insert a space before all uppercase letters  
-    // and then capitalize the first letter of each word
+   
     return str.replace(/([A-Z])/g, ' $1').trim().split(' ')
               .map(word => word.charAt(0).toUpperCase() + word.slice(1))
               .join(' ');
