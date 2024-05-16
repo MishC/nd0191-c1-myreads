fb30e32ac1fdb79e06f7a3bf85e54554634f8fa4
MishC <michaela.chovancova@gmail.com>
2024-05-16 12:55:59 +0200

    added function filledCategory

--- starter/src/components/Search.js
+++ starter/src/components/Search.js
@@ -13,24 +13,22 @@ const [inputValue, setInputValue]=useState([]);
 const [searchedBooks,setSearchedBooks]=useState([]);
 const [myBooks,setMyBooks]=useState([]);
 
-/*const findIndex=(book)=>{allbooks.findIndex(bookMain=>bookMain.id===book.id)};
-const changeCategory=(books)=>{books.map((book1) => 
-  findIndex(book1)?book1.shelf=allbooks[findIndex(book1)].shelf:book1)};
-const foundBook=(books)=>books.filter(book=>allbooks.includes(book.id))
-const intersection = allBooks.filter(book => searchedBooks.includes(book));
+const fillCategory = async (books) => {
+  try {
+      const results = await books.map(book1 => {
+          const foundBook = allbooks.find(book2 => book2.id === book1.id);
+          if (foundBook) {
+              return { ...book1, shelf: foundBook.shelf };
+          } else {
+              return {...book1,shelf:"none"}; 
+          }
+      });
 
-const fillCategory = () => {
-  try{
-  const books = books.map((book1, ind) => {const index=allbooks.findIndex(book2=>book1.id===book2.id)
-    index?book1=allbooks[index]:book1});
+      setSearchedBooks(results);
+  } catch (error) {
+      console.error("An error occurred:", error);
   }
-  catch (error) {
-    // Handle any errors that occur during execution
-    console.error("An error occurred:", error);
-}
-};*/
-
- 
+};
 
 const handleChange = async (e) => {
   
@@ -40,11 +38,9 @@ const handleChange = async (e) => {
   try{
     if (newValue.trim() !== "") { 
       const data = await search(newValue);
-            
-      setSearchedBooks(data); 
-      setMyBooks(allbooks.filter(book => searchedBooks.includes(book.id)));
-      setSearchedBooks(searchedBooks.filter(book=>!allbooks.includes(book.id)));   
-      
+       fillCategory(data);     
+     
+     console.log("2:",searchedBooks);
       
   } else {
       setSearchedBooks([]); 
@@ -78,14 +74,10 @@ return(
 <div className="search-books-results">
   
   <div>
-   <DisplayBooks category="none" myBooks={searchedBooks} selectCategory={selectCategory}/>
+   <Home allbooks={searchedBooks} selectCategory={selectCategory}/>
 
 </div></div>:<div>{}</div>
 }
-{myBooks.length>1&&<div>
-   <Home allbooks={myBooks} selectCategory={selectCategory}/>
-
-</div>}
 </div>
 
 )}
