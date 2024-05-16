42dc5428a20d993ed13bf72b3904ad3b6c6a2988
MishC <michaela.chovancova@gmail.com>
2024-05-16 11:29:26 +0200

    Update Search.js

--- starter/src/components/Search.js
+++ starter/src/components/Search.js
@@ -4,24 +4,57 @@ import DisplayBooks from "./DisplayBooks";
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 
+import Home from "./Home";
 
 
-const Search=({selectCategory})=>{
+
+const Search=({selectCategory,allbooks})=>{
 const [inputValue, setInputValue]=useState([]);
 const [searchedBooks,setSearchedBooks]=useState([]);
+const [myBooks,setMyBooks]=useState([]);
+
+/*const findIndex=(book)=>{allbooks.findIndex(bookMain=>bookMain.id===book.id)};
+const changeCategory=(books)=>{books.map((book1) => 
+  findIndex(book1)?book1.shelf=allbooks[findIndex(book1)].shelf:book1)};
+const foundBook=(books)=>books.filter(book=>allbooks.includes(book.id))
+const intersection = allBooks.filter(book => searchedBooks.includes(book));
+
+const fillCategory = () => {
+  try{
+  const books = books.map((book1, ind) => {const index=allbooks.findIndex(book2=>book1.id===book2.id)
+    index?book1=allbooks[index]:book1});
+  }
+  catch (error) {
+    // Handle any errors that occur during execution
+    console.error("An error occurred:", error);
+}
+};*/
 
  
 
 const handleChange = async (e) => {
+  
   const newValue = e.target.value;
-  setInputValue(newValue);
-  if (newValue.trim() !== "") { 
+  
+    setInputValue(newValue);
+  try{
+    if (newValue.trim() !== "") { 
       const data = await search(newValue);
-      setSearchedBooks(data || []); 
+            
+      setSearchedBooks(data); 
+      setMyBooks(allbooks.filter(book => searchedBooks.includes(book.id)));
+      setSearchedBooks(searchedBooks.filter(book=>!allbooks.includes(book.id)));   
+      
+      
   } else {
       setSearchedBooks([]); 
   }
+}
+catch (error) {console.error("Error occured:", {error})};
+
 };
+
+
 
 
 return(
@@ -41,14 +74,21 @@ return(
   </div>
 </div> 
 {(searchedBooks[0] && searchedBooks.length > 0 && Array.isArray(searchedBooks))?
+
 <div className="search-books-results">
   
   <div>
-   <DisplayBooks category="" allbooks={searchedBooks} selectCategory={selectCategory}/>
+   <DisplayBooks category="none" myBooks={searchedBooks} selectCategory={selectCategory}/>
 
 </div></div>:<div>{}</div>
 }
-</div>)}
+{myBooks.length>1&&<div>
+   <Home allbooks={myBooks} selectCategory={selectCategory}/>
+
+</div>}
+</div>
+
+)}
 Search.propTypes = {
     selectCategory: PropTypes.func.isRequired,
   };
