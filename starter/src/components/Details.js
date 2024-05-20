7b1725171c92533ffc9e7d23b56ca82c8e8c83be
MishC <michaela.chovancova@gmail.com>
2024-05-20 14:35:48 +0200

    usenavigate in Details

--- starter/src/components/Details.js
+++ starter/src/components/Details.js
@@ -1,9 +1,12 @@
+import { Link, useNavigate } from "react-router-dom";
+
 const Details = ({book}) =>{
-    console.log(book);
+    const navigate = useNavigate();
+
+    
     return(
 <div className="Details">
 <div>{book.title}</div>
-
-
+<button onClick={() => navigate(-1)}>Go Back</button>
 </div>)};
 export default Details;
